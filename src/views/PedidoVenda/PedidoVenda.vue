7ebf5cd47190709e2a6f4ba83d0655661cc87a2c
Denny Philipp Gonçalves <denny.philipp@outlook.com>
2021-02-28 21:22:29 -0300

    Adicionado html bases nas novas paginas

--- src/views/PedidoVenda/PedidoVenda.vue
+++ src/views/PedidoVenda/PedidoVenda.vue
@@ -1,9 +1,32 @@
 <template>
-  <div>
-      <h1>Pedido Venda</h1>
+  <div class="animated fadeIn">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="card">
+          <header class="card-header">
+            <div class="d-flex">
+              <strong class="align-self-center">Pedidos Venda</strong>
+              <a
+                class="ml-auto btn btn-primary"
+                href="/#/pedido-venda/novo"
+                title="Adicionar novo pedido de venda"
+              >
+                Adicionar
+              </a>
+            </div>
+          </header>
+          <div v-if="loading" class="loading-container">
+            <RotateSquare
+              class="loading-position animated fadeIn"
+              size="60px"
+            ></RotateSquare>
+          </div>
+          <div v-else class="card-body"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
   name: "PedidoVenda",
--- src/views/Produto/Produto.vue
+++ src/views/Produto/Produto.vue
@@ -1,6 +1,30 @@
 <template>
-  <div>
-      <h1>Produto</h1>
+  <div class="animated fadeIn">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="card">
+          <header class="card-header">
+            <div class="d-flex">
+              <strong class="align-self-center">Produtos</strong>
+              <a
+                class="ml-auto btn btn-primary"
+                href="/#/produto/novo"
+                title="Adicionar novo produto"
+              >
+                Adicionar
+              </a>
+            </div>
+          </header>
+          <div v-if="loading" class="loading-container">
+            <RotateSquare
+              class="loading-position animated fadeIn"
+              size="60px"
+            ></RotateSquare>
+          </div>
+          <div v-else class="card-body"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
