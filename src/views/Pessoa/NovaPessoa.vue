99f356e416bf1bf40fa30b32cbfaf8250986c638
Denny Philipp Gonçalves <denny.philipp@outlook.com>
2021-03-13 17:58:05 -0300

    Adicionado arquivos base para nova pessoa

--- src/views/Pessoa/NovaPessoa.vue
+++ src/views/Pessoa/NovaPessoa.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
-      <h1>Nova Pessoa</h1>
+    <h1>Nova Pessoa</h1>
   </div>
 </template>
 
 <script>
+import TipoPessoa from "../../enums/TipoPessoa";
+import TipoSexo from "../../enums/TipoSexo";
+
 export default {
   name: "NovaPessoa",
   components: {},
   data() {
-    return {};
+    return {
+      tipoPessoa: 0,
+      nome: "",
+      nomeCompleto: "",
+      nacionalidade: "",
+      dataNascimento: "",
+      estadoCivil: "",
+      telefone: "",
+      telefone2: "",
+      tipoSexo: 3,
+      email: "",
+      observacao: ""
+    };
   },
   mounted() {},
   methods: {}
