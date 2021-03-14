428b7e4b0e68211630d29001b0eb8cdb162869c0
Denny Philipp Gonçalves <denny.philipp@outlook.com>
2021-03-13 21:10:33 -0300

    Adicionado campos no cadastro de pessoa

--- src/views/Pessoa/NovaPessoa.vue
+++ src/views/Pessoa/NovaPessoa.vue
@@ -34,7 +34,9 @@
 
                 <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
                   <div class="form-group">
-                    <label for>* Nome</label>
+                    <label for>{{
+                      isPessoaJuridica() ? "* Fantasia" : "* Nome"
+                    }}</label>
                     <input
                       v-model="viewModel.nome"
                       class="form-control"
@@ -45,32 +47,32 @@
                   </div>
                 </div>
 
-                <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                <div
+                  class="col-sm-12 col-md-4 col-lg-4 col-xl-4"
+                  v-if="isPessoaJuridica()"
+                >
                   <div class="form-group">
-                    <label for>Nome Completo</label>
+                    <label for>* Razão Social</label>
                     <input
                       v-model="viewModel.nomeCompleto"
                       class="form-control"
                       type="text"
-                      placeholder="Digite o nome completo"
+                      placeholder="Digite a razão social"
                       required
                     />
                   </div>
                 </div>
                 <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
                   <div class="form-group">
-                    <label for>* E-mail</label>
+                    <label for>E-mail</label>
                     <input
                       v-model="viewModel.email"
                       class="form-control"
                       type="email"
                       placeholder="Digite o e-mail"
-                      required
                     />
                   </div>
                 </div>
-              </div>
-              <div class="row">
                 <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
                   <div class="form-group">
                     <label for>Nacionalidade</label>
@@ -83,9 +85,13 @@
                     />
                   </div>
                 </div>
+              </div>
+              <div class="row">
                 <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
                   <div class="form-group">
-                    <label for>Data Nascimento</label>
+                    <label for>{{
+                      isFuncionario() ? "Data Nascimento" : "Data Fundação"
+                    }}</label>
                     <input
                       v-model="viewModel.dataNascimento"
                       class="form-control"
@@ -95,7 +101,10 @@
                     />
                   </div>
                 </div>
-                <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                <div
+                  class="col-sm-12 col-md-3 col-lg-3 col-xl-3"
+                  v-if="isFuncionario()"
+                >
                   <div class="form-group">
                     <label for>Estado Civil</label>
                     <input
@@ -107,7 +116,10 @@
                     />
                   </div>
                 </div>
-                <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                <div
+                  class="col-sm-12 col-md-3 col-lg-3 col-xl-3"
+                  v-if="isFuncionario()"
+                >
                   <div class="form-group">
                     <label for>Sexo</label>
                     <b-form-select
@@ -117,13 +129,48 @@
                   </div>
                 </div>
               </div>
+              <div class="row">
+                <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                  <div class="form-group">
+                    <label for>Telefone</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      v-model="viewModel.telefone"
+                    />
+                  </div>
+                </div>
+                <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                  <div class="form-group">
+                    <label for>Telefone</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      v-model="viewModel.telefone2"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                  <div class="form-group">
+                    <label for>Observação</label>
+                    <b-form-textarea
+                      v-model="viewModel.observacao"
+                      rows="4"
+                      max-rows="12"
+                      placeholder="Observações gerais..."
+                    ></b-form-textarea>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <button class="btn btn-success mr-2" type="submit">Salvar</button>
           <button
             class="btn btn-secondary"
             type="reset"
-            @click="$router.push('/usuario')"
+            @click="$router.push('/pessoa')"
           >
             Voltar
           </button>
@@ -169,9 +216,94 @@ export default {
       }
     };
   },
-  mounted() {},
+  created() {
+    let id = this.$route.params.id;
+    if (id) this.Obter(id);
+  },
   methods: {
-    ValidarForm() {}
+    isPessoaJuridica() {
+      return (
+        this.viewModel.tipoPessoa == TipoPessoa.Fornecedor ||
+        this.viewModel.tipoPessoa == TipoPessoa.Cliente
+      );
+    },
+    isFuncionario() {
+      return this.viewModel.tipoPessoa == TipoPessoa.Funcionario;
+    },
+    ValidarForm(evt) {
+      evt.preventDefault();
+      if (this.viewModel.id !== this.$store.state.emptyGuid) this.Editar();
+      else this.Novo();
+    },
+    Obter(id) {
+      this.loading = true;
+      this.$http({
+        url: "pessoa/obter/" + id,
+        method: "GET"
+      })
+        .then((resposta) => {
+          this.loading = false;
+          this.viewModel = resposta.data;
+        })
+        .catch((erro) => {
+          this.loading = false;
+          this.$notify({
+            data: erro.response.data.erros,
+            type: "warn",
+            duration: 10000
+          });
+        });
+    },
+    Novo() {
+      this.loading = true;
+      this.$http({
+        url: "pessoa/novo",
+        data: this.viewModel,
+        method: "POST"
+      })
+        .then(() => {
+          this.loading = false;
+          this.$router.push("/pessoa");
+          this.$notify({
+            data: ["Pessoa cadastrado com sucesso."],
+            type: "success",
+            duration: 10000
+          });
+        })
+        .catch((erro) => {
+          this.loading = false;
+          this.$notify({
+            data: erro.response.data.erros,
+            type: "warn",
+            duration: 10000
+          });
+        });
+    },
+    Editar() {
+      this.loading = true;
+      this.$http({
+        url: "pessoa/editar",
+        data: this.viewModel,
+        method: "PUT"
+      })
+        .then(() => {
+          this.loading = false;
+          this.$router.push("/pessoa");
+          this.$notify({
+            data: ["Pessoa editado com sucesso."],
+            type: "success",
+            duration: 10000
+          });
+        })
+        .catch((erro) => {
+          this.loading = false;
+          this.$notify({
+            data: erro.response.data.erros,
+            type: "warn",
+            duration: 10000
+          });
+        });
+    }
   }
 };
 </script>
