b62e4cb7a12ed06800e69b73d87a1671f40311c2
Denny Philipp Gonçalves <denny.philipp@outlook.com>
2021-03-20 23:21:14 -0300

    Implementado collapse componentes novo enderço e novo documento

--- src/views/Pessoa/NovoDocumento.vue
+++ src/views/Pessoa/NovoDocumento.vue
@@ -11,123 +11,138 @@
       <div class="row">
         <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
           <div class="card">
-            <header class="card-header">
-              <strong class="align-self-center">Documentos</strong>
-            </header>
-            <div class="card-body">
-              <div class="row">
-                <div class="col">
-                  <div class="form-group">
-                    <small>Campos com * são de preenchimento obrigatório</small>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
-                  <div class="form-group">
-                    <label for="">* Número</label>
-                    <input
-                      class="form-control"
-                      type="text"
-                      v-model="viewModel.numero"
-                      required
-                    />
-                  </div>
-                </div>
-                <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
-                  <div class="form-group">
-                    <label for="">Validade</label>
-                    <input
-                      class="form-control"
-                      type="date"
-                      v-model="viewModel.validade"
-                    />
-                  </div>
-                </div>
-                <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
-                  <div class="form-group">
-                    <label for>* Tipo</label>
-                    <b-form-select
-                      v-model="viewModel.tipoDocumentoId"
-                      :options="tipos"
-                      required
-                    ></b-form-select>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
-                  <div class="form-group">
-                    <label for>Observação</label>
-                    <b-form-textarea
-                      v-model="viewModel.observacao"
-                      rows="4"
-                      max-rows="12"
-                      placeholder="Observações gerais..."
-                    ></b-form-textarea>
-                  </div>
-                </div>
-              </div>
-              <div class="btn-toolbar mb-3" role="toolbar">
-                <div class="btn-group" role="group">
-                  <button class="btn btn-success mr-2" type="submit">
-                    Salvar
-                  </button>
-                </div>
-                <div class="btn-group" role="group">
-                  <button
-                    class="btn btn-secondary"
-                    type="reset"
-                    @click="$router.push('/pessoa')"
-                  >
-                    Voltar
-                  </button>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-12">
-                  <b-table
-                    :hover="true"
-                    responsive
-                    :items="itens"
-                    :fields="fields"
-                    striped
-                    :per-page="itensPorPagina"
-                    show-empty
-                    empty-text="Nenhum documento encontrado."
-                  >
-                    <template v-slot:empty="scope">
-                      <h4>{{ scope.emptyText }}</h4>
-                    </template>
+            <header class="card-header" @click="abrir = !abrir">
+              <div class="d-flex">
+                <strong class="align-self-center">Documento(s)</strong>
+                <small class="ml-2 mt-1">Clique para abrir/esconder</small>
 
-                    <template v-slot:cell(acoes)="data">
-                      <div class="btn-group-sm">
-                        <b-button
-                          variant="warning"
-                          style="margin-right: 10px"
-                          title="Editar"
-                          @click="Obter(data.item.id)"
-                        >
-                          <i class="fa fa-edit text-black"></i>
-                        </b-button>
-                        <b-button
-                          variant="danger"
-                          title="Remover"
-                          @click="Remover(data.item.id)"
-                        >
-                          <i class="fas fa-trash-alt text-black"></i>
-                        </b-button>
-                      </div>
-                    </template>
-                  </b-table>
-                  <b-pagination
-                    v-model="pagina"
-                    :total-rows="total"
-                    :per-page="itensPorPagina"
-                    align="right"
-                    size="md"
-                    class="mt-2"
-                  ></b-pagination>
+                <i
+                  :class="
+                    abrir
+                      ? 'ml-auto mt-1 fas fa-chevron-up'
+                      : 'ml-auto mt-1 fas fa-chevron-down'
+                  "
+                ></i>
+              </div>
+            </header>
+            <div :class="abrir ? 'collapse-show' : 'collapse'">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col">
+                    <div class="form-group">
+                      <small
+                        >Campos com * são de preenchimento obrigatório</small
+                      >
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                    <div class="form-group">
+                      <label for="">* Número</label>
+                      <input
+                        class="form-control"
+                        type="text"
+                        v-model="viewModel.numero"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                    <div class="form-group">
+                      <label for="">Validade</label>
+                      <input
+                        class="form-control"
+                        type="date"
+                        v-model="viewModel.validade"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                    <div class="form-group">
+                      <label for>* Tipo</label>
+                      <b-form-select
+                        v-model="viewModel.tipoDocumentoId"
+                        :options="tipos"
+                        required
+                      ></b-form-select>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <div class="form-group">
+                      <label for>Observação</label>
+                      <b-form-textarea
+                        v-model="viewModel.observacao"
+                        rows="4"
+                        max-rows="12"
+                        placeholder="Observações gerais..."
+                      ></b-form-textarea>
+                    </div>
+                  </div>
+                </div>
+                <div class="btn-toolbar mb-3" role="toolbar">
+                  <div class="btn-group" role="group">
+                    <button class="btn btn-success mr-2" type="submit">
+                      Salvar
+                    </button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button
+                      class="btn btn-secondary"
+                      type="reset"
+                      @click="$router.push('/pessoa')"
+                    >
+                      Voltar
+                    </button>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <b-table
+                      :hover="true"
+                      responsive
+                      :items="itens"
+                      :fields="fields"
+                      striped
+                      :per-page="itensPorPagina"
+                      show-empty
+                      empty-text="Nenhum documento encontrado."
+                    >
+                      <template v-slot:empty="scope">
+                        <h4>{{ scope.emptyText }}</h4>
+                      </template>
+
+                      <template v-slot:cell(acoes)="data">
+                        <div class="btn-group-sm">
+                          <b-button
+                            variant="warning"
+                            style="margin-right: 10px"
+                            title="Editar"
+                            @click="Obter(data.item.id)"
+                          >
+                            <i class="fa fa-edit text-black"></i>
+                          </b-button>
+                          <b-button
+                            variant="danger"
+                            title="Remover"
+                            @click="Remover(data.item.id)"
+                          >
+                            <i class="fas fa-trash-alt text-black"></i>
+                          </b-button>
+                        </div>
+                      </template>
+                    </b-table>
+                    <b-pagination
+                      v-model="pagina"
+                      :total-rows="total"
+                      :per-page="itensPorPagina"
+                      align="right"
+                      size="md"
+                      class="mt-2"
+                    ></b-pagination>
+                  </div>
                 </div>
               </div>
             </div>
@@ -159,6 +174,7 @@ export default {
       total: 0,
       itensPorPagina: 5,
       itens: [],
+      abrir: false,
       fields: [
         { key: "numero", label: "Número", sortable: true },
         { key: "tipoDocumento", label: "Tipo", sortable: true },
--- src/views/Pessoa/NovoEndereco.vue
+++ src/views/Pessoa/NovoEndereco.vue
@@ -7,210 +7,217 @@
       ></RotateSquare>
     </div>
     <form v-else @submit="ValidarForm">
-      <!-- <div class="row">
-        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12"> -->
       <div class="card">
-        <header class="card-header">
+        <header class="card-header" @click="abrir = !abrir">
           <div class="d-flex">
-            <strong class="align-self-center">Endereço</strong>
+            <strong class="align-self-center">Endereço(s)</strong>
+            <small class="ml-2 mt-1">Clique para abrir/esconder</small>
+
+            <i
+              :class="
+                abrir
+                  ? 'ml-auto mt-1 fas fa-chevron-up'
+                  : 'ml-auto mt-1 fas fa-chevron-down'
+              "
+            ></i>
           </div>
         </header>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
-              <div class="form-group">
-                <label for>* Tipo</label>
-                <b-form-select
-                  v-model="viewModel.tipoEnderecoId"
-                  :options="tipos"
-                  required
-                ></b-form-select>
+        <div :class="abrir ? 'collapse-show' : 'collapse'">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                <div class="form-group">
+                  <label for>* Tipo</label>
+                  <b-form-select
+                    v-model="viewModel.tipoEnderecoId"
+                    :options="tipos"
+                    required
+                  ></b-form-select>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-2">
-              <div class="form-group">
-                <label>* CEP</label>
-                <input
-                  v-model="viewModel.cep"
-                  type="number"
-                  class="form-control"
-                  @change="ConsultarCep"
-                />
+            <div class="row">
+              <div class="col-sm-12 col-md-6 col-lg-3 col-xl-2">
+                <div class="form-group">
+                  <label>* CEP</label>
+                  <input
+                    v-model="viewModel.cep"
+                    type="number"
+                    class="form-control"
+                    @change="ConsultarCep"
+                  />
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                <div class="form-group">
+                  <label>* Rua</label>
+                  <input
+                    v-model="viewModel.logradouro"
+                    type="text"
+                    class="form-control"
+                    placeholder="Rua, Av..."
+                  />
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3">
+                <div class="form-group">
+                  <label>* Número</label>
+                  <input
+                    v-model="viewModel.numero"
+                    type="text"
+                    class="form-control"
+                  />
+                </div>
               </div>
             </div>
-            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
-              <div class="form-group">
-                <label>* Rua</label>
-                <input
-                  v-model="viewModel.logradouro"
-                  type="text"
-                  class="form-control"
-                  placeholder="Rua, Av..."
-                />
+            <div class="row">
+              <div class="col-sm-12 col-md-4 col-lg-4 col-xl-2">
+                <div class="form-group">
+                  <label>* Bairro</label>
+                  <input
+                    v-model="viewModel.bairro"
+                    type="text"
+                    class="form-control"
+                  />
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                <div class="form-group">
+                  <label>* Cidade</label>
+                  <input
+                    v-model="viewModel.cidade"
+                    type="text"
+                    class="form-control"
+                  />
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-6 col-lg-5 col-xl-5">
+                <div class="form-group">
+                  <label>Complemento</label>
+                  <input
+                    v-model="viewModel.complemento"
+                    type="text"
+                    class="form-control"
+                    placeholder="apartamento, casa, fundos..."
+                  />
+                </div>
               </div>
             </div>
-            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3">
-              <div class="form-group">
-                <label>* Número</label>
-                <input
-                  v-model="viewModel.numero"
-                  type="text"
-                  class="form-control"
-                />
+            <div class="row">
+              <div class="col-sm-12 col-md-6 col-lg-3 col-xl-2">
+                <div class="form-group">
+                  <label for>* UF</label>
+                  <select v-model="viewModel.uf" class="form-control" required>
+                    <option disabled selected value>Selecione...</option>
+                    <option value="AC">AC</option>
+                    <option value="AL">AL</option>
+                    <option value="AP">AP</option>
+                    <option value="AM">AM</option>
+                    <option value="BA">BA</option>
+                    <option value="CE">CE</option>
+                    <option value="DF">DF</option>
+                    <option value="ES">ES</option>
+                    <option value="GO">GO</option>
+                    <option value="MA">MA</option>
+                    <option value="MT">MT</option>
+                    <option value="MS">MS</option>
+                    <option value="MG">MG</option>
+                    <option value="PA">PA</option>
+                    <option value="PB">PB</option>
+                    <option value="PR">PR</option>
+                    <option value="PE">PE</option>
+                    <option value="PI">PI</option>
+                    <option value="RJ">RJ</option>
+                    <option value="RN">RN</option>
+                    <option value="RS">RS</option>
+                    <option value="RO">RO</option>
+                    <option value="RR">RR</option>
+                    <option value="SC">SC</option>
+                    <option value="SP">SP</option>
+                    <option value="SE">SE</option>
+                    <option value="TO">TO</option>
+                  </select>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-6 col-lg-3 col-xl-2">
+                <div class="form-group">
+                  <label for>* País</label>
+                  <input
+                    type="text"
+                    v-model="viewModel.pais"
+                    class="form-control"
+                    required
+                  />
+                </div>
               </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-12 col-md-4 col-lg-4 col-xl-2">
-              <div class="form-group">
-                <label>* Bairro</label>
-                <input
-                  v-model="viewModel.bairro"
-                  type="text"
-                  class="form-control"
-                />
+            <div class="btn-toolbar mb-3" role="toolbar">
+              <div class="btn-group" role="group">
+                <button class="btn btn-success mr-2" type="submit">
+                  Salvar
+                </button>
+              </div>
+              <div class="btn-group" role="group">
+                <button
+                  class="btn btn-secondary"
+                  type="reset"
+                  @click="$router.push('/pessoa')"
+                >
+                  Voltar
+                </button>
               </div>
             </div>
-            <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-              <div class="form-group">
-                <label>* Cidade</label>
-                <input
-                  v-model="viewModel.cidade"
-                  type="text"
-                  class="form-control"
-                />
-              </div>
-            </div>
-            <div class="col-sm-12 col-md-6 col-lg-5 col-xl-5">
-              <div class="form-group">
-                <label>Complemento</label>
-                <input
-                  v-model="viewModel.complemento"
-                  type="text"
-                  class="form-control"
-                  placeholder="apartamento, casa, fundos..."
-                />
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-2">
-              <div class="form-group">
-                <label for>* UF</label>
-                <select v-model="viewModel.uf" class="form-control" required>
-                  <option disabled selected value>Selecione...</option>
-                  <option value="AC">AC</option>
-                  <option value="AL">AL</option>
-                  <option value="AP">AP</option>
-                  <option value="AM">AM</option>
-                  <option value="BA">BA</option>
-                  <option value="CE">CE</option>
-                  <option value="DF">DF</option>
-                  <option value="ES">ES</option>
-                  <option value="GO">GO</option>
-                  <option value="MA">MA</option>
-                  <option value="MT">MT</option>
-                  <option value="MS">MS</option>
-                  <option value="MG">MG</option>
-                  <option value="PA">PA</option>
-                  <option value="PB">PB</option>
-                  <option value="PR">PR</option>
-                  <option value="PE">PE</option>
-                  <option value="PI">PI</option>
-                  <option value="RJ">RJ</option>
-                  <option value="RN">RN</option>
-                  <option value="RS">RS</option>
-                  <option value="RO">RO</option>
-                  <option value="RR">RR</option>
-                  <option value="SC">SC</option>
-                  <option value="SP">SP</option>
-                  <option value="SE">SE</option>
-                  <option value="TO">TO</option>
-                </select>
-              </div>
-            </div>
-            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-2">
-              <div class="form-group">
-                <label for>* País</label>
-                <input
-                  type="text"
-                  v-model="viewModel.pais"
-                  class="form-control"
-                  required
-                />
-              </div>
-            </div>
-          </div>
-          <div class="btn-toolbar mb-3" role="toolbar">
-            <div class="btn-group" role="group">
-              <button class="btn btn-success mr-2" type="submit">Salvar</button>
-            </div>
-            <div class="btn-group" role="group">
-              <button
-                class="btn btn-secondary"
-                type="reset"
-                @click="$router.push('/pessoa')"
-              >
-                Voltar
-              </button>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-12">
-              <b-table
-                :hover="true"
-                responsive
-                :items="itens"
-                :fields="fields"
-                striped
-                :per-page="itensPorPagina"
-                show-empty
-                empty-text="Nenhum endereço encontrado."
-              >
-                <template v-slot:empty="scope">
-                  <h4>{{ scope.emptyText }}</h4>
-                </template>
+            <div class="row">
+              <div class="col-12">
+                <b-table
+                  :hover="true"
+                  responsive
+                  :items="itens"
+                  :fields="fields"
+                  striped
+                  :per-page="itensPorPagina"
+                  show-empty
+                  empty-text="Nenhum endereço encontrado."
+                >
+                  <template v-slot:empty="scope">
+                    <h4>{{ scope.emptyText }}</h4>
+                  </template>
 
-                <template v-slot:cell(acoes)="data">
-                  <div class="btn-group-sm">
-                    <b-button
-                      variant="warning"
-                      style="margin-right: 10px"
-                      title="Editar"
-                      @click="Obter(data.item.id)"
-                    >
-                      <i class="fa fa-edit text-black"></i>
-                    </b-button>
-                    <b-button
-                      variant="danger"
-                      title="Remover"
-                      @click="Remover(data.item.id)"
-                    >
-                      <i class="fas fa-trash-alt text-black"></i>
-                    </b-button>
-                  </div>
-                </template>
-              </b-table>
-              <b-pagination
-                v-model="pagina"
-                :total-rows="total"
-                :per-page="itensPorPagina"
-                align="right"
-                size="md"
-                class="mt-2"
-              ></b-pagination>
+                  <template v-slot:cell(acoes)="data">
+                    <div class="btn-group-sm">
+                      <b-button
+                        variant="warning"
+                        style="margin-right: 10px"
+                        title="Editar"
+                        @click="Obter(data.item.id)"
+                      >
+                        <i class="fa fa-edit text-black"></i>
+                      </b-button>
+                      <b-button
+                        variant="danger"
+                        title="Remover"
+                        @click="Remover(data.item.id)"
+                      >
+                        <i class="fas fa-trash-alt text-black"></i>
+                      </b-button>
+                    </div>
+                  </template>
+                </b-table>
+                <b-pagination
+                  v-model="pagina"
+                  :total-rows="total"
+                  :per-page="itensPorPagina"
+                  align="right"
+                  size="md"
+                  class="mt-2"
+                ></b-pagination>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
-      <!-- </div>
-      </div> -->
     </form>
   </div>
-  <!-- </div> -->
 </template>
 
 <script>
@@ -233,6 +240,7 @@ export default {
       total: 0,
       itensPorPagina: 5,
       itens: [],
+      abrir: false,
       fields: [
         { key: "tipo", label: "Tipo", sortable: true },
         { key: "cep", label: "CEP", sortable: true },
