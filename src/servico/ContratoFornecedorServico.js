f467aadbec08522de6f5737e9e28f3aab6e66439
Beto Oliveira <davidoliveira@outlook.com>
2021-04-04 14:16:42 -0300

    Contrato Fornecedor

--- src/servico/ContratoFornecedorServico.js
+++ src/servico/ContratoFornecedorServico.js
@@ -1,48 +1,47 @@
 import Vue from "vue";
 class DocumentoServico {
-    Obter(id) {
-        return Vue.prototype.$http({
-            url: "fornecedorProduto/obter/" + id,
-            method: "GET"
-        });
-    }
+  Obter(id) {
+    return Vue.prototype.$http({
+      url: "pessoacontrato/obter/" + id,
+      method: "GET"
+    });
+  }
 
-    ObterGrid(pagina, itensPorPagina, produtoId) {
-        return Vue.prototype.$http({
-          url:
-          "/fornecedorProduto/obter-grid?pagina=" +
-          pagina +
-          "&ItensPorPagina=" +
-          itensPorPagina +
-          "&produtoId=" +
-          produtoId,
-        method: "GET"
-        });
-    }
+  ObterGrid(pagina, itensPorPagina, contratoId) {
+    return Vue.prototype.$http({
+      url:
+        "/pessoacontrato/obter-grid?pagina=" +
+        pagina +
+        "&ItensPorPagina=" +
+        itensPorPagina +
+        "&contratoId=" +
+        contratoId,
+      method: "GET"
+    });
+  }
 
-    Remover(id) {
-        return Vue.prototype.$http({
-            url: "fornecedorProduto/remover/" + id,
-            method: "DELETE"
-        });
-    }
+  Remover(id) {
+    return Vue.prototype.$http({
+      url: "pessoacontrato/remover/" + id,
+      method: "DELETE"
+    });
+  }
 
-    Novo(viewModel) {
-        return Vue.prototype.$http({
-            url: "fornecedorProduto/novo",
-            data: viewModel,
-            method: "POST"
-        });
-    }
+  Novo(viewModel) {
+    return Vue.prototype.$http({
+      url: "pessoacontrato/novo",
+      data: viewModel,
+      method: "POST"
+    });
+  }
 
-    Editar(viewModel) {
-        return Vue.prototype.$http({
-            url: "fornecedorProduto/editar",
-            data: viewModel,
-            method: "PUT"
-        });
-    }
-
-};
+  Editar(viewModel) {
+    return Vue.prototype.$http({
+      url: "pessoacontrato/editar",
+      data: viewModel,
+      method: "PUT"
+    });
+  }
+}
 
 export default new DocumentoServico();
