7f87ee08c556e236da03ee090ffe6571a204fe22
Denny Philipp Gonçalves <denny.philipp@outlook.com>
2021-04-18 21:02:44 -0300

    Adicionado campos data de entrada nome de pai e mae em cadastro pessoa

--- src/util/DateTime.js
+++ src/util/DateTime.js
@@ -1,5 +1,8 @@
 const DateTime = {
     formatar(data) {
+        if (!data)
+            return null;
+            
         return new Date(data)
             .toISOString()
             .split("T")[0];
