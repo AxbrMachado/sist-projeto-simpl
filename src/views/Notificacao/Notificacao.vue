8d57dda0c9a41d0265632cfcaa69404b9f7216e7
Denny Philipp Gonçalves <denny.philipp@outlook.com>
2021-05-11 20:47:46 -0300

    Adicionado página de notificação no header

---
+++ src/views/Notificacao/Notificacao.vue
@@ -0,0 +1,35 @@
+<template>
+  <a class="btn" @click="AbrirModal()" title="Visualizar notificações">
+    <i class="fas fa-bell"></i>
+    <b-modal
+      v-model="abrir"
+      title="Notificações"
+      size="lg"
+      scrollable
+      hide-footer
+    >
+      <h2>Notificação</h2>
+    </b-modal>
+  </a>
+</template>
+
+<script>
+import RotateSquare from "../../components/RotateSquare";
+
+export default {
+  components: { RotateSquare },
+
+  data() {
+    return {
+      abrir: false
+    };
+  },
+  mounted() {},
+
+  methods: {
+    AbrirModal() {
+      this.abrir = true;
+    }
+  }
+};
+</script>
