e6d3d8dfb1321a71ce9fd1d6ce2468a659a02922
Denny Philipp Gonçalves <denny.philipp@outlook.com>
2021-05-24 20:28:40 -0300

    Adicionado gupo usuario serviço

---
+++ src/servico/GrupoUsuarioServico.js
@@ -0,0 +1,37 @@
+import Vue from "vue";
+class GrupoUsuarioServico {
+  Obter(id) {
+    return Vue.prototype.$http({
+      url: "grupousuario/obter/" + id,
+      method: "GET"
+    });
+  }
+
+  ObterGrid(pagina, itensPorPagina) {
+    return Vue.prototype.$http({
+      url:
+        "/grupousuario/obter-grid?pagina=" +
+        pagina +
+        "&ItensPorPagina=" +
+        itensPorPagina,
+      method: "GET"
+    });
+  }
+
+  Remover(id) {
+    return Vue.prototype.$http({
+      url: "grupousuario/remover/" + id,
+      method: "DELETE"
+    });
+  }
+
+  Novo(viewModel) {
+    return Vue.prototype.$http({
+      url: "grupousuario/novo",
+      data: viewModel,
+      method: "POST"
+    });
+  }
+}
+
+export default new GrupoUsuarioServico();
