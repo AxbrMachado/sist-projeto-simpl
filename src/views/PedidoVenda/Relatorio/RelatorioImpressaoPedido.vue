58ac408da2a99c56b92a74d905165bde92ddf192
Denny Philipp Gonçalves <denny.philipp@outlook.com>
2021-10-10 19:04:36 -0300

    Inicio relatorio impressão

--- src/views/PedidoVenda/Relatorio/RelatorioImpressaoPedido.vue
+++ src/views/PedidoVenda/Relatorio/RelatorioImpressaoPedido.vue
@@ -1,13 +1,47 @@
 <template>
   <div class="animated fadeIn">
-   
+    <div v-for="(item, index) in relatorio.clientes" :key="index">
+      <div class="row">
+        <span>Data: ____/____/______ </span>
+      </div>
+      <div class="row">
+        <span>Unidade Escolar: {{ item.nome }}</span>
+      </div>
+      <div class="row">
+        <b-table
+          striped
+          hover
+          :items="item.produtos"
+          :fields="fields"
+        ></b-table>
+      </div>
+
+      <div class="row">
+        <p>Obs: {{ item.observacao }}</p>
+      </div>
+      <div class="row" style="margin: 10px; padding-top: 30px">
+        <table class="table">
+          <tr>
+            <td align="center" valign="center" style="border-top: solid 1px;">
+              Resp. Entrega
+            </td>
+            
+            <td align="center" valign="center" style="border: none;">
+              
+            </td>
+            <td align="center" valign="center" style="border-top: solid 1px">
+              Resp. Recebimento
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import RotateSquare from "../../../components/RotateSquare";
 import PedidoServico from "../../../servico/PedidoServico";
-
 
 export default {
   name: "RelatorioImpressaoPedido",
@@ -17,17 +51,11 @@ export default {
   data() {
     return {
       loading: false,
-      itens: [],
+      relatorio: {},
       fields: [
-        { key: "nome", label: "Nome", sortable: true },
-        { key: "email", label: "E-mail", sortable: true },
-        { key: "ativo", label: "Ativo", sortable: true },
-        {
-          key: "acoes",
-          label: "Ações",
-          sortable: false,
-          thClass: "center, wd-120-px"
-        }
+        { key: "produto", label: "Itens" },
+        { key: "quantidade", label: "Quantidade" },
+        { key: "unidadeMedida", label: "Unid." }
       ]
     };
   },
@@ -37,21 +65,21 @@ export default {
   },
   methods: {
     Obter(pedidoId) {
-      this.loading = false;
-
-    //   PedidoServico.RelatorioImpressao(pedidoId)
-    //     .then((response) => {
-    //       this.loading = false;
-    //       console.log(response);
-    //     })
-    //     .catch((erro) => {
-    //       this.loading = false;
-    //       this.$notify({
-    //         data: erro.response.data.erros,
-    //         type: "warn",
-    //         duration: 5000
-    //       });
-    //     });
+      this.loading = true;
+      PedidoServico.RelatorioImpressao(pedidoId)
+        .then((response) => {
+          this.loading = false;
+          this.relatorio = response.data;
+          console.log(response);
+        })
+        .catch((erro) => {
+          this.loading = false;
+          this.$notify({
+            data: erro.response.data.erros,
+            type: "warn",
+            duration: 5000
+          });
+        });
     }
   }
 };
